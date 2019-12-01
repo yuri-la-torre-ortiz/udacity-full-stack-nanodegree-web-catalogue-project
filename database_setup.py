09ab040ba54c505515cadcd2cf3cc4ec68a5c222
Yuri La Torre Ortiz <yuri.la.torre.ortiz@protonmail.ch>
2019-12-01 19:21:14 +0100

    Add initial commit

---
+++ database_setup.py
@@ -0,0 +1,59 @@
+from sqlalchemy import Column, ForeignKey, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import relationship
+from sqlalchemy import create_engine
+
+Base = declarative_base()
+
+
+class User(Base):
+    __tablename__ = 'user'
+
+    id = Column(Integer, primary_key=True)
+    name = Column(String(250), nullable=False)
+    email = Column(String(250), nullable=False)
+    picture = Column(String(250))
+
+
+class Genre(Base):
+    __tablename__ = 'genre'
+
+    id = Column(Integer, primary_key=True)
+    name = Column(String(250), nullable=False)
+
+    @property
+    def serialize(self):
+        """Return object data in easily serializeable format"""
+        return {
+            'name': self.name,
+            'id': self.id,
+        }
+
+
+class Film(Base):
+    __tablename__ = 'film'
+
+    id = Column(Integer, primary_key=True)
+    title = Column(String(80), nullable=False)
+    year = Column(Integer)
+    description = Column(String(250))
+    poster_image = Column(String(250))
+    user_id = Column(Integer, ForeignKey('user.id'))
+    user = relationship(User)
+
+    @property
+    def serialize(self):
+        """Return object data in easily serializeable format"""
+        return {
+            'id': self.id,
+            'title': self.title,
+            'year': self.year,
+            'description': self.description,
+            'poster_image': self.poster_image,
+        }
+
+
+engine = create_engine('sqlite:///films.db')
+
+
+Base.metadata.create_all(engine)
